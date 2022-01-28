09c4a9505a3d29bbf0f366d6bd5a7433c4e32715
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2022-01-28 08:27:12 -0300

    wip

--- App.js
+++ App.js
@@ -5,10 +5,6 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/HomeScreen';
 
 import Constants from 'expo-constants';
-<<<<<<< HEAD
-=======
-import  firebase from 'firebase/app'
->>>>>>> 640bea9bfc9ab211388793bf0edbfac55cef0242
 import firebaseConfig from './firebaseConfig';
 import { AuthProvider } from './components/InfoProvider';
 
