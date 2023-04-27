3ade358d41d3e6ee7c70882257480438ed2752a6
lucianovilela <lucianovilela@gmail.com>
2023-04-27 16:59:50 +0000

    correcoes

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './components/HomeScreen';
 import { AuthProvider } from './components/InfoProvider';
-import "./firebaseConfig";
+
 
 export default function App() {
   
